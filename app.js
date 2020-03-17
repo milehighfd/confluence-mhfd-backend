2a951e363c0b81f75e8c419a500018299da66fdf
Gabriel Rivera Safadi <gabriel.rivera.safadi@gmail.com>
2020-03-17 17:41:03 -0400

    Add cors headers.

--- app.js
+++ app.js
@@ -77,7 +77,10 @@ app.use(cookieParser());
 // add CORS headers
 app.use(function(res, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT");
+  res.header("Access-Control-Max-Age", "86400");
   next();
 });
 
