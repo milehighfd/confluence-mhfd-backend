3bce64ca902148bad7e02b9050dc4b1dc7e67ad2
Lua <xdaddisxd@gmail.com>
2020-03-19 19:32:56 -0400

    signup

--- routes/users.route.js
+++ routes/users.route.js
@@ -2,29 +2,34 @@ const express = require('express');
 const User = require('../models/user.model');
 const router = express.Router();
 const auth = require('../auth/auth');
+const { validator } = require('../utils/utils');
 
 router.get('/', async (req, res, next) => {
   const users = await User.find({});
   res.send(users);
 });
 
-router.post('/', async (req, res) => {
+router.post('/signup', validator(['firstName', 'lastName', 'designation', 'email', 'organization', 'password']), async (req, res) => {
   try {
-    var user = new User(req.body);
-    const userRegistered = User.findByEmail(user.email); //async User.findByEmail(user.email);
-    //console.log(existeEmail);
-    if(!userRegistered) {
-      res.status(500).send({message: 'The e-mail has already been registered'});
+    const user = new User(req.body);
+    const foundUser = await User.count({email: user.email}); 
+    if(foundUser) {
+      res.status(422).send({message: 'The e-mail has already been registered'});
     } else {
-      await user.save();
-      const token = await user.generateAuthToken();
-      res.status(201).send({
-        user,
-        token
-      });
+      if (/^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/.test(user.email)) {
+        user.name = user.firstName + " " + user.lastName;
+        await user.save();
+        const token = await user.generateAuthToken();
+        res.status(201).send({
+          user,
+          token
+        });
+      } else {
+        return res.status(400).send('You entered an invalid email direction');
+      }
     }
   } catch (error) {
-    res.status(400).send(error);
+    res.status(500).send(error);
   }
 });
 
