598268579077e0cbbb3a65ac8fe528c95b54d283
Lua <xdaddisxd@gmail.com>
2020-04-30 10:50:26 -0400

    download csv library

--- routes/logActivity.route.js
+++ routes/logActivity.route.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const auth = require('../auth/auth');
 const logActivityService = require('../services/logActivity.service');
 const logger = require('../config/logger');
+const json2csv = require('json2csv');
 
 router.post('/save', auth, async (req, res) => {
   try {
@@ -35,5 +36,27 @@ router.get('/get-all', auth, async (req, res) => {
   	res.status(500).send({error: error});
   }
 });
+router.get('/csv', auth, async (req, res) => {
+  try {
+    let activity = await logActivityService.getLogActivities(1, 1000000, 'registerDate', '-1');
+    console.log(activity)
+      //TODO Bladi fix please 
+    activity = activity.map(element => {
+      element.user = element.user.length ? element.user[0].name : element.userId[0];
+      return element;
+    });
+      console.log(activity);
+    res.set('Content-Type', 'application/octet-stream');
+    //TODO Bladi put cool names
+    const fields = ['registerDate', 'city', 'user'];
+    const data = json2csv.parse(activity, {fields});
+    console.log(activity);
+    res.attachment('activity.csv');
+    res.send(data);
+  } catch(error) {
+    logger.error(error);
+    res.status(500).send({error: error})
+  }
 
+});
 module.exports = router;
