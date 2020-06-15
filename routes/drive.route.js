b42539719ec6a57d8a4e903a7ff74891755f507a
Bladimir Calcina <bcalcinac@outlook.com>
2020-06-15 18:57:53 -0400

    get images from google drive

--- routes/drive.route.js
+++ routes/drive.route.js
@@ -44,7 +44,7 @@ router.get('/get-images-drive', async (req, res) => {
   }
 });
 
-var j = schedule.scheduleJob('5 53 14 * * *', function(){
+var j = schedule.scheduleJob('1 3 * * * *', function(){
   
   try {
     if (!fs.existsSync(IMAGES_FROM_GOOGLE_DRIVE)) {
