ce49fbe0e6f462e35ce8a92797f9921486fd73ed
Lua <xdaddisxd@gmail.com>
2020-08-04 15:22:29 -0400

    geocoder endpoint

--- routes/map.route.js
+++ routes/map.route.js
@@ -61,4 +61,76 @@ router.post('/', async (req, res) => {
   });
 });
 
+
+router.get('/search/:query', async (req, res) => {
+  const query = req.params.query;
+  const to_url = encodeURIComponent(query);
+  const map = `https://api.mapbox.com/geocoding/v5/mapbox.places/${to_url}.json?bbox=-109.4424900706,36.6070426751,-101.6263840793,41.2535776532&access_token=pk.eyJ1IjoibWlsZWhpZ2hmZCIsImEiOiJjazRqZjg1YWQwZTN2M2RudmhuNXZtdWFyIn0.oU_jVFAr808WPbcVOFnzbg`;
+  const promises = [];
+  promises.push(new Promise((resolve, reject) => {
+    console.log(map);
+    https.get(map, response => {   
+      if (response.statusCode == 200) {
+        let str = '';
+        response.on('data', function (chunk) {
+          str += chunk;
+        });
+        response.on('end', function () {          
+          const places = JSON.parse(str).features;
+          const filteredPlaces = places.map(ele => {
+            return {
+              text: ele.text,
+              place_name: ele.place_name,
+              center: ele.center,
+              type: 'geocoder'
+          }});
+          resolve(filteredPlaces);
+        });
+      } else {
+        resolve([]);
+      }
+    }).on('error', err => {
+      resolve([]);
+    })})
+  );  
+  let sql = `SELECT ST_x(ST_line_interpolate_point(st_makeline(st_linemerge(the_geom)), 0.5)) as x, ST_y(ST_line_interpolate_point(st_makeline(st_linemerge(the_geom)), 0.5)) as y, str_name FROM streams WHERE  str_name ILIKE '%${query}%' AND ST_IsEmpty(the_geom) = false group by str_name`;
+  console.log('el query ' , sql);
+  sql =  encodeURIComponent(sql);
+  const URL = `https://denver-mile-high-admin.carto.com/api/v2/sql?q=${sql}&api_key=${CARTO_TOKEN}`;
+  promises.push(new Promise((resolve, reject) => {
+    https.get(URL, response => {   
+      if (response.statusCode == 200) {
+        let str = '';
+        response.on('data', function (chunk) {
+          str += chunk;
+        });
+        response.on('end', function () {
+          console.log(JSON.parse(str));
+          const places = JSON.parse(str).rows;
+          const filteredPlaces = places.map(ele => {
+            return {
+              text: ele.str_name,
+              place_name: 'stream',
+              center: [ele.x, ele.y],
+              type: 'stream'
+          }});
+          resolve(filteredPlaces);
+        });
+      } else {
+        console.log('status ', response.statusCode, URL);
+        resolve([]);
+      }
+    }).on('error', err => {
+      console.log('failed call to ', URL, 'with error ', err);
+      resolve([]);
+    })})
+  );  
+  const all = await Promise.all(promises);
+  const answer = [];
+  for(const data of all) {
+    answer.push(...data);
+  }
+  res.send(answer);
+});
+
 module.exports = (router);
