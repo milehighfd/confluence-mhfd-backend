881d56370fd0f8672411b01bace32706c14125c3
Lua <xdaddisxd@gmail.com>
2020-12-17 14:32:05 -0400

    zoom area

--- routes/zoomarea.route.js
+++ routes/zoomarea.route.js
@@ -50,6 +50,54 @@ router.get('/', async (req, res) => {
   }
 })
 
+
+router.get('/complete', async (req, res) => {
+  try {
+    const newProm = new Promise((resolve, reject) => {
+      const sql = `select cartodb_id, aoi, filter, ST_AsGeoJSON(the_geom) from mhfd_zoom_to_areas order by aoi`;
+      const URL = encodeURI(`https://denver-mile-high-admin.carto.com/api/v2/sql?q=${sql}&api_key=${CARTO_TOKEN}`);
+      let result = [];
+      https.get(URL, response => {
+        if (response.statusCode === 200) {
+          let str = '';
+          response.on('data', function (chunk) {
+            str += chunk;
+          });
+          response.on('end', async function () {
+            let firstElement = {};
+            let arrayAux = [];
+            for (let row of JSON.parse(str).rows) {
+              if (row.cartodb_id === 1) {
+                firstElement = {
+                  aoi: row.aoi,
+                  filter: row.filter,
+                  coordinates: JSON.parse(row.st_asgeojson).coordinates
+                }
+              } else {
+                arrayAux.push({
+                  aoi: row.aoi,
+                  filter: row.filter,
+                  coordinates: JSON.parse(row.st_asgeojson).coordinates
+                })
+              }
+            }
+            let result = [];
+            result.push(firstElement);
+            result = result.concat(arrayAux);
+            resolve(result);
+          })
+        }
+      })
+    })
+
+    const respuesta = await newProm;
+    res.status(200).send(respuesta);
+  } catch (error) {
+    logger.error(error);
+    res.status(500).send({ error: error }).send({ error: 'Error with C connection' });
+  }
+})
+
 router.get('/get-zoom-filter', async (req, res) => {
   try {
     const newProm = new Promise((resolve, reject) => {
