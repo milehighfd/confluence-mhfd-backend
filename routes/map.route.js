6da50de36a063c86574d892c047de466445ab345
Lua <xdaddisxd@gmail.com>
2020-12-18 16:31:11 -0400

    bbox component endpoint

--- routes/map.route.js
+++ routes/map.route.js
@@ -140,5 +140,85 @@ router.get('/search/:query', async (req, res) => {
   });
   res.send(answer);
 });
+const components = [
+  { key: 'grade_control_structure', value: 'Grade Control Structure' },
+  { key: 'pipe_appurtenances', value: 'Pipe Appurtenances' },
+  { key: 'special_item_point', value: 'Special Item Point' },
+  { key: 'special_item_linear', value: 'Special Item Linear' },
+  { key: 'special_item_area', value: 'Special Item Area' },
+  { key: 'channel_improvements_linear', value: 'Channel Improvements Linear' },
+  { key: 'channel_improvements_area', value: 'Channel Improvements Area' },
+  { key: 'removal_line', value: 'Removal Line' },
+  { key: 'removal_area', value: 'Removal Area' },
+  { key: 'storm_drain', value: 'Storm Drain' },
+  { key: 'detention_facilities', value: 'Detention Facilities' },
+  { key: 'maintenance_trails', value: 'Maintenance Trails' },
+  { key: 'land_acquisition', value: 'Land Acquisition' },
+  { key: 'landscaping_area', value: 'Landscaping Area' }
+];
+router.get('/bbox-components', async (req, res) => {
+  const id = req.query.id;
+  const table = req.query.table;
+  let field = 'projectid';
+  if (table === 'problems') {
+    field = 'problemid';
+  }
+  const promises = [];
+  for (const element of components) {
+    const component = element.key;
+    let sql = `SELECT ST_extent(${component}.the_geom) as bbox FROM "denver-mile-high-admin".${component} 
+    where ${component}.${field} = ${id}`;
+    sql = encodeURIComponent(sql);
+    const URL = `https://denver-mile-high-admin.carto.com/api/v2/sql?q=${sql}&api_key=${CARTO_TOKEN}`;
+    promises.push(new Promise((resolve, reject) => {
+      https.get(URL, response => {   
+        if (response.statusCode == 200) {
+          let str = '';
+          response.on('data', function (chunk) {
+            str += chunk;
+          });
+          response.on('end', function () {
+            const rows = JSON.parse(str).rows;
+            if (rows[0].bbox != null) {
+              rows[0].bbox = rows[0].bbox.replace('BOX(', '').replace(')', '').replace(/ /g, ',').split(',');
+            }
+            resolve({bbox: rows[0].bbox});
+          });
+        } else {
+          console.log('status ', response.statusCode, URL);
+          resolve([]);
+        }
+      }).on('error', err => {
+        console.log('failed call to ', URL, 'with error ', err);
+        resolve([]);
+      })})
+    );  
+  }
+  const all = await Promise.all(promises);
+  const answer = [];
+  for(const data of all) {
+    if (data.bbox != null) { 
+      answer.push(data);
+    }
+  }
+  let [minLat, minLng, maxLat, maxLng] = [Infinity, Infinity, -Infinity, -Infinity];
+  for (const bbox of answer) {
+    const coords = bbox.bbox;
+    if (coords[0] < minLat) {
+      minLat = coords[0];
+    }
+    if (coords[1] < minLng) {
+      minLng = coords[1];
+    }
+    if (coords[2] > maxLat) {
+      maxLat = coords[2];
+    }
+    if (coords[3] > maxLng) {
+      maxLng = coords[3];
+    }
+  }
+  const polygon = [[[minLat, minLng], [minLat, maxLng], [maxLat, maxLng], [maxLat, minLng], [minLat, minLng]]];
+  res.send(polygon);
+});
 
 module.exports = (router);
