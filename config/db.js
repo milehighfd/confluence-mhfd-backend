d5452a2768134c96a08f3f6e79a740c2d70d09ec
pachonjcl <pachonjcl@gmail.com>
2021-05-24 18:29:54 -0400

    remove task

--- config/db.js
+++ config/db.js
@@ -22,7 +22,6 @@ db.project = require('../models/project.model.js')(sequelize, Sequelize);
 db.user = require('../models/user.model.js')(sequelize, Sequelize);
 db.component = require('../models/component.model.js')(sequelize, Sequelize);
 db.attachment = require('../models/attachment.model.js')(sequelize, Sequelize);
-db.task = require('../models/task.model.js')(sequelize, Sequelize);
 db.coordinate = require('../models/coordinate.model.js')(sequelize, Sequelize);
 db.logActivity = require('../models/logActivity.model.js')(sequelize, Sequelize);
 db.favorites = require('../models/favorites.model.js')(sequelize, Sequelize);
