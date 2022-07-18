7504de6849dbf8f0373df2cf2503fcb467ca590f
jose <pachonjcl@gmail.com>
2022-07-18 16:55:51 -0400

    feat: add update configuration

--- routes/configuration.route.js
+++ routes/configuration.route.js
@@ -6,12 +6,31 @@ const router = express.Router();
 
 router.get('/:key', async (req, res) => {
   const key = req.params.key;
-  let configurations = await Configuration.findOne({
+  let configuration = await Configuration.findOne({
     where: {
       key
     }
   });
-  res.send(configurations);
+  res.send(configuration);
+});
+
+router.put('/:key', async (req, res) => {
+  const key = req.params.key;
+  let configuration = await Configuration.findOne({
+    where: {
+      key
+    }
+  });
+  if (configuration === null) {
+    const value = req.body.value;
+    configuration.value = value;
+    await configuration.save();
+    res.send(configuration);
+  } else {
+    res.status(404).send({
+      error: 'Configuration not found'
+    });
+  }
 });
 
 module.exports = router;
