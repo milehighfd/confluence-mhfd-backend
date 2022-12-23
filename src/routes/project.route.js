8be4c850cfe4ace1d1ae5736150e54c646c80121
Lua <xdaddisxd@gmail.com>
2022-12-23 14:44:41 -0400

    get projects

--- src/routes/project.route.js
+++ src/routes/project.route.js
@@ -1,17 +1,39 @@
 import express from 'express';
 import db from 'bc/config/db.js';
 import logger from 'bc/config/logger.js';
-
 const Projects = db.project;
+const ProjectPartner = db.projectPartner;
+const BusinessAssociates = db.businessAssociates;
 const router = express.Router();
 
 const listProjects = async (req, res) => {
-  const { offset = 0, limit = 10 } = req.query;
+  const { offset = 0, limit = 20 } = req.query;
   let projects = await Projects.findAll({
     limit,
     offset,
     include: { all: true, nested: true }
+  }).map(result => result.dataValues);
+  const SPONSOR_TYPE = 11; // maybe this can change in the future
+  const ids = projects.map((p) => p.project_id);
+  const project_partners = await ProjectPartner.findAll({
+    where: {
+      project_id: ids,
+      code_partner_type_id: SPONSOR_TYPE,
+    },
+    include: { all: true, nested: true }
+  }).map(result => result.dataValues).map(res => { 
+    return {...res, business_associate: res.business_associate.dataValues }
   });
+  projects = projects.map((project) => {
+    const partners = project_partners.filter((partner) => partner.project_id === project.project_id);
+    let sponsor = null;
+    if (partners.length) {
+      sponsor = partners[0].business_associate.business_associate_name;
+    } 
+    console.log({...project, sponsor });
+    return  {...project, sponsor: sponsor };
+  });
+  // xconsole.log(project_partners);
   logger.info('projects being called');
   res.send(projects);
 };
