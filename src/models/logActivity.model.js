a0ec10fec37477716e648c244128a32a98736466
Donwilo <burgoadanilson@gmail.com>
2023-02-03 14:31:25 -0800

    Fix logActivity model

--- src/models/logActivity.model.js
+++ src/models/logActivity.model.js
@@ -1,17 +1,17 @@
 
 export default (sequelize, DataType) => {
-  const LogActivity = sequelize.define('log_activity', {
-    _id: {
-      type: DataType.UUID,
-      defaultValue: DataType.UUIDV4,
-      primaryKey: true
-    },
+  const LogActivity = sequelize.define('log_activity', {    
     registerDate: {
       type: DataType.DATE
     },
+    user_id_DNU: {
+      type: DataType.UUID,
+      defaultValue: DataType.UUIDV4,      
+    },
     user_id: {
       type: DataType.INTEGER,
-      allowNull: false
+      allowNull: false,
+      primaryKey: true
     },
     activityType: {
       type: DataType.ENUM,
