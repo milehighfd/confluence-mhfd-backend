6d3d8f802972735cbe64c79cbd01f725c4f7e0ad
Lua <xdaddisxd@gmail.com>
2023-02-10 12:22:53 -0400

    update to get projects with deprecated version

--- src/routes/cardfilters.route.js
+++ src/routes/cardfilters.route.js
@@ -253,11 +253,11 @@ const getFilters = async (req, res) => {
   res.send({'all': 'too well', data});
 }
 const getProjectsComplete = async (req, res) => {
-  const projects = await projectService.getProjectsComplete();
+  const projects = await projectService.getProjects();
   res.send({projects: projects});
 }
 const getProjectsPromise = async (req, res) => {
-  const projects = await projectService.getProjects();
+  const projects = await projectService.getProjectsDeprecated();
   res.send({projects: projects});
 }
 router.get('/projectspromise', getProjectsPromise);
