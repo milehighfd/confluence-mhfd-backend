d153810a103eaa8d75aba2b07dacb4841b7a8b66
Donwilo <burgoadanilson@gmail.com>
2023-03-02 16:09:40 -0400

    new order for phasetype

--- src/routes/phasetype.route.js
+++ src/routes/phasetype.route.js
@@ -1,19 +1,24 @@
 import express from 'express';
 import db from 'bc/config/db.js';
+import sequelize from 'sequelize';
 
 const router = express.Router();
 const CodePhaseType = db.codePhaseType;
 const CodeStatusType = db.codeStatusType;
+const Op = sequelize.Op;
 
 router.post('/', async (req, res) => {
   const code_project_type_id = req.body.tabKey;
   try {
     const codePhaseType = await CodePhaseType.findAll({
       where: {
-        code_project_type_id: code_project_type_id
+        code_project_type_id: code_project_type_id,
+        code_status_type_id: {
+          [Op.gt]: 4
+        }        
       },      
       include: { model: CodeStatusType},
-      order: ['code_project_type_id','code_status_type_id'],
+      order: ['code_project_type_id','code_status_type_id','phase_ordinal_position' ],
     });
     return res.send(codePhaseType);
   } catch(error) {
