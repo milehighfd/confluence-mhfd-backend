7e16b14665f190b77a3fa01116fe57a2b24cfd2a
Lua <xdaddisxd@gmail.com>
2023-03-06 11:31:39 -0400

    add problem favorite model

---
+++ src/models/problem_favorites.model.js
@@ -0,0 +1,40 @@
+export default (sequelize, DataType) => {
+  const ProblemFavorite = sequelize.define('problem_favorite', {
+
+    project_favorite_id: {
+      type: DataType.INTEGER,
+      primaryKey: true
+    },
+    // user_character_id: {
+    //   type: DataType.STRING,
+    //   defaultValue: 'defaultValue',
+    // },
+    user_id: {
+      type: DataType.INTEGER,
+    },
+    project_id: {
+      type: DataType.INTEGER,
+    },
+    project_table_name: {
+      type: DataType.STRING,
+    },
+    created_date: {
+      type: DataType.DATE,
+    },
+    modified_date: {
+      type: DataType.DATE,
+    },
+    last_modified_by: {
+      type: DataType.STRING,
+    },
+    created_by: {
+      type: DataType.STRING,
+    }
+  }, {
+    freezeTableName: true,
+    tableName: 'problem_favorite',
+    createdAt: false,
+    updatedAt: false
+  });
+  return ProblemFavorite;
+}
