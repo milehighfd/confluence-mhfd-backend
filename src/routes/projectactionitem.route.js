f84ce0c720b539029ca4d043c66608c77b1594db
Donwilo <burgoadanilson@gmail.com>
2023-03-08 16:05:38 -0400

    Create project action item route

---
+++ src/routes/projectactionitem.route.js
@@ -0,0 +1,47 @@
+import express from 'express';
+import db from 'bc/config/db.js';
+import sequelize from 'sequelize';
+
+const router = express.Router();
+const CodePhaseType = db.codePhaseType;
+const CodeRuleActionItem = db.codeRuleActionItem;
+const ProjectActionItem = db.projectActionItem;
+const Op = sequelize.Op;
+
+router.get('/', async (req, res) => {
+  let boardProjects = await ProjectActionItem.findAll({      
+  });
+  res.send(boardProjects);
+});
+
+router.post('/', async (req, res) => {
+  const  name  = req.body;  
+  try {
+    const group = await ProjectActionItem.create(name);
+    return res.send(group);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send(error);
+  }
+});
+
+router.delete('/', async (req, res) => {
+  const  name  = req.body;  
+  try {
+    const project =  await ProjectActionItem.destroy({
+      where: { code_rule_action_item_id: name.code_rule_action_item_id ,
+      project_id: name.project_id  },
+    });
+    if (project) {      
+      res.status(200).send('Destroyed'); 
+    } else {  
+      res.status(200).send('Not Found');  
+    }
+   
+  } catch (error) {
+    console.log(error);
+    res.status(500).send(error);
+  }
+});
+
+export default router;
