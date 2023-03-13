b5d02f79d02b6a89ae18928dbaa72d93635ed5f0
Donwilo <burgoadanilson@gmail.com>
2023-03-13 12:19:45 -0400

    Add start date and end date to endpoint

--- src/routes/projectStatus.route.js
+++ src/routes/projectStatus.route.js
@@ -27,9 +27,11 @@ router.put('/', async (req, res) => {
   const code_phase_type_id = req.body.code_phase_type_id;
   const project_id = req.body.project_id;
   const comments = req.body.comments;
+  const actual_start_date = req.body.actual_start_date;
+  const actual_end_date = req.body.actual_end_date;
   try {
     const result = await projectStatus.update(
-      { comments: comments },
+      { comments: comments, actual_start_date: actual_start_date, actual_end_date: actual_end_date },
       { where: { project_id: project_id, code_phase_type_id: code_phase_type_id } }
     )
     return res.send(result);
