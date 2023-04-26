779bd44c0c84f6012334d0978cd6ded5599ca6d9
cesarLima1 <cesmaulim@gmail.com>
2023-04-26 18:01:35 -0400

    fix filter issues

--- src/routes/cardfilters.route.js
+++ src/routes/cardfilters.route.js
@@ -43,132 +43,138 @@ const getFilters = async (req, res) => {
   let projectsFilterId = await projectService.getProjects2(null, null, 1, null, body);
   let projects = await projectService.getProjects(null, null, projectsFilterId, null, null);
 
-  projects = await projectsByFilters(projects, body);
-  if (bounds) {
-    const ids = await getIdsInBbox(bounds);
-    projects = projects.filter((p) => ids.includes(p.project_id));
-  }
-  
+  // projects = await projectsByFilters(projects, body);
+
+  // commented is not applying bounds yet TODO: verify ids and filter 
+  // if (bounds) {
+  //   const ids = await getIdsInBbox(bounds);
+  //   // console.log('idssssss', ids)
+  //   projects = projects.filter((p) => { console.log('pppppp',ids.includes(p.dataValues.project_id)); return ids.includes(p.dataValues.project_id)});
+  // }
+
+
   const toMatch = [];
-  if (body.completedyear && body.completedyear.length) { // don't touch
+//   if (body.completedyear && body.completedyear.length) { // don't touch
 
-  }
-  if (body.contractor && body.contractor.length) { //
-    const ids = projects.filter((project) => {
-      let has = false;
-      project?.contractors?.forEach(con => {
-        con?.business?.forEach((cc) => {
-          body.contractor.forEach((c) => {
-            has |= (cc.business_associates_id === c);
-          });  
-        });
-      });
-      return has;
-    }).map((project) => project.project_id);
-    data.contractor = data.contractor.filter((con) => body.contractor.includes(con.id));
-    toMatch.push(ids);
+//   }
+//   if (body.contractor && body.contractor.length) { //
+//     const ids = projects.filter((project) => {
+//       let has = false;
+//       project?.contractors?.forEach(con => {
+//         con?.business?.forEach((cc) => {
+//           body.contractor.forEach((c) => {
+//             has |= (cc.business_associates_id === c);
+//           });  
+//         });
+//       });
+//       return has;
+//     }).map((project) => project.project_id);
+//     data.contractor = data.contractor.filter((con) => body.contractor.includes(con.id));
+//     toMatch.push(ids);
     
-  }
-  if (body.consultant && body.consultant.length) { //
-    const ids = projects.filter((project) => {
-      let has = false;
-      project?.consultants?.forEach(con => {
-        con?.consultant?.forEach((cc) => {
-          body.consultant.forEach((c) => {
-            has |= (cc.business_associates_id === c);
-          });  
-        });
-      });
-      return has;
-    }).map((project) => project.project_id);
-    data.consultant = data.consultant.filter((con) => body.consultant.includes(con.id));
-    toMatch.push(ids);
-  }
-  if (body.county && body.county.length) { //x
-    const ids = projects.filter((project) => {
-      return body.county.includes(project?.county?.codeStateCounty?.state_county_id);
-    }).map((project) => project.project_id);
-    data.county = data.county.filter((con) => body.county.includes(con.id));
-    toMatch.push(ids);
-  }
-  if (body.creator && body.creator.length) { // pause for now, guess is the user?
+//   }
+//   if (body.consultant && body.consultant.length) { //
+//     const ids = projects.filter((project) => {
+//       let has = false;
+//       project?.consultants?.forEach(con => {
+//         con?.consultant?.forEach((cc) => {
+//           body.consultant.forEach((c) => {
+//             has |= (cc.business_associates_id === c);
+//           });  
+//         });
+//       });
+//       return has;
+//     }).map((project) => project.project_id);
+//     data.consultant = data.consultant.filter((con) => body.consultant.includes(con.id));
+//     toMatch.push(ids);
+//   }
+//   if (body.county && body.county.length) { //x
+      
+//     const ids = projects.filter((project) => {
+//       return body.county.includes(project?.county?.codeStateCounty?.state_county_id);
+//     }).map((project) => project.project_id);
+//     console.log('asddsafasdf',ids)
+//     data.county = data.county.filter((con) => body.county.includes(con.id));
+//     toMatch.push(ids);
+//   }
+//   if (body.creator && body.creator.length) { // pause for now, guess is the user?
 
-  }
-  if (body.totalcost && body.totalcost.length) {
-    const ids = projects.filter((project) => {
-      return project?.estimatedCost?.cost >= body.totalcost[0] && project?.estimatedCost?.cost <= body.totalcost[1];
-    }).map((project) => {
-      return project.project_id
-    });
-    data.estimatedCost = data.estimatedCost?.filter((con) => body.estimatedcost.includes(con.id));
-    toMatch.push(ids);
-  }
-  if (body.jurisdiction && body.jurisdiction.length) { //
-    const ids = projects.filter((project) => {
-      return body.jurisdiction.includes(project?.localGoverment?.codeLocalGoverment?.code_local_government_id)
-    }).map((project) => {
-      return project.project_id
-    });
-    data.jurisdiction = data.jurisdiction.filter((con) => body.jurisdiction.includes(con.id));
-    toMatch.push(ids);
-  }
-  if (body.keyword && body.keyword.length) { // idk
+//   }
+//   if (body.totalcost && body.totalcost.length) {
+//     const ids = projects.filter((project) => {
+//       return project?.estimatedCost?.cost >= body.totalcost[0] && project?.estimatedCost?.cost <= body.totalcost[1];
+//     }).map((project) => {
+//       return project.project_id
+//     });
+//     data.estimatedCost = data.estimatedCost?.filter((con) => body.estimatedcost.includes(con.id));
+//     toMatch.push(ids);
+//   }
+//   if (body.jurisdiction && body.jurisdiction.length) { //
+//     const ids = projects.filter((project) => {
+//       return body.jurisdiction.includes(project?.localGoverment?.codeLocalGoverment?.code_local_government_id)
+//     }).map((project) => {
+//       return project.project_id
+//     });
+//     data.jurisdiction = data.jurisdiction.filter((con) => body.jurisdiction.includes(con.id));
+//     toMatch.push(ids);
+//   }
+//   if (body.keyword && body.keyword.length) { // idk
 
-  }
-  if (body.lgmanager && body.lgmanager.length) { // pause for now
+//   }
+//   if (body.lgmanager && body.lgmanager.length) { // pause for now
 
-  }
-  if (body.mhfdmanager && body.mhfdmanager.length) { // pause for now
+//   }
+//   if (body.mhfdmanager && body.mhfdmanager.length) { // pause for now
 
-  }
-  if (body.order) { // idk don't look important
-  }
-  if (body.projecttype && body.projecttype.length) { // maybe we will have problems with the names
-    const ids = projects.filter((project) => {
-      return body.projecttype.includes(
-        project?.project_status?.code_phase_type?.code_project_type?.code_project_type_id
-      );
-    }).map((project) => project.project_id);
-    data.projecttype = data.projecttype.filter((con) => body.projecttype.includes(con.id));
-    toMatch.push(ids);
-  }
-  if (body.startyear && body.startyear.length) { // don't touch
+//   }
+//   if (body.order) { // idk don't look important
+//   }
+//   if (body.projecttype && body.projecttype.length) { // maybe we will have problems with the names
+//     const ids = projects.filter((project) => {
+//       return body.projecttype.includes(
+//         project?.project_status?.code_phase_type?.code_project_type?.code_project_type_id
+//       );
+//     }).map((project) => project.project_id);
+//     data.projecttype = data.projecttype.filter((con) => body.projecttype.includes(con.id));
+//     toMatch.push(ids);
+//   }
+//   if (body.startyear && body.startyear.length) { // don't touch
 
-  }
-  if (body.status && body.status.length) { // 
-    const ids = projects.filter((project) => {
-      return body.status.includes(project?.project_status?.code_phase_type?.code_status_type?.code_status_type_id);
-    }).map((project) => {
-      return project.project_id
-    });
-    // data.status = data.status.filter((con) => body.status.includes(con.id));
-    toMatch.push(ids);
-  }
-  if (body.streamname && body.streamname.length) { //
-    const ids = projects.filter((project) => {
-      const has = false;
-      project.streams?.stream?.forEach((stream) => {
-        has |= body.streamname.includes(stream.stream_id);
-      });
-      return has;
-    }).map((project) => {
-      return project.project_id
-    });
-    data.streamname = data.streamname.filter((con) => body.streamname.includes(con.id));
-    toMatch.push(ids);
-  }
-  if (body.servicearea && body.servicearea.length) {
-    const ids = projects.filter((project) => {
-      return project?.project_service_areas.some( elem => body.servicearea.includes(elem?.CODE_SERVICE_AREA?.code_service_area_id) )
-    }).map((project) => {
-      return project.project_id
-    });
-    data.servicearea = data.servicearea.filter((con) => body.servicearea.includes(con.id));
-    toMatch.push(ids);
-  }
+//   }
+//   if (body.status && body.status.length) { // 
+//     const ids = projects.filter((project) => {
+//       return body.status.includes(project?.project_status?.code_phase_type?.code_status_type?.code_status_type_id);
+//     }).map((project) => {
+//       return project.project_id
+//     });
+//     // data.status = data.status.filter((con) => body.status.includes(con.id));
+//     toMatch.push(ids);
+//   }
+//   if (body.streamname && body.streamname.length) { //
+//     const ids = projects.filter((project) => {
+//       const has = false;
+//       project.streams?.stream?.forEach((stream) => {
+//         has |= body.streamname.includes(stream.stream_id);
+//       });
+//       return has;
+//     }).map((project) => {
+//       return project.project_id
+//     });
+//     data.streamname = data.streamname.filter((con) => body.streamname.includes(con.id));
+//     toMatch.push(ids);
+//   }
+//   if (body.servicearea && body.servicearea.length) {
+//     const ids = projects.filter((project) => {
+//       return project?.project_service_areas.some( elem => body.servicearea.includes(elem?.CODE_SERVICE_AREA?.code_service_area_id) )
+//     }).map((project) => {
+//       return project.project_id
+//     });
+//     data.servicearea = data.servicearea.filter((con) => body.servicearea.includes(con.id));
+//     toMatch.push(ids);
+//   }
   const toCount = {};
   projects.forEach((project) => {
-    toCount[project.project_id] = 0;
+    toCount[project.dataValues.project_id] = 0;
   });
   const finalProjects = [];
   toMatch.forEach((array) => {
@@ -191,15 +197,19 @@ const getFilters = async (req, res) => {
   });
   data.jurisdiction.forEach((d) => {
     d.counter = projects.reduce((pre, current) => {
-      if (current?.localGoverment?.codeLocalGoverment?.code_local_government_id === d.id) {
+      if (current?.project_local_governments?.some( pc => pc?.CODE_LOCAL_GOVERNMENT?.code_local_government_id === +d.id)) {
         return pre + 1;
       }
+
+      // if (current?.localGoverment?.codeLocalGoverment?.code_local_government_id === d.id) {
+      //   return pre + 1;
+      // }
       return pre;
     }, 0);
   });
   data.county.forEach((d) => {
     d.counter = projects.reduce((pre, current) => {
-      if (current?.project_counties?.some( pc => pc?.CODE_STATE_COUNTY?.state_county_id === +d.id)) {
+      if (current?.project_counties?.some( pc => pc?.CODE_STATE_COUNTY?.dataValues.state_county_id === +d.id)) {
         return pre + 1;
       }
       return pre;
