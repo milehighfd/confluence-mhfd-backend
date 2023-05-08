60b2ec54f635e436d85e1d2e2795befdb6b9456b
dottyy <dottykurmi123@gmail.com>
2023-05-08 11:16:05 -0400

    edit style pdf

--- src/services/mapgallery.print.service.js
+++ src/services/mapgallery.print.service.js
@@ -386,7 +386,12 @@ export const newPrintProject = async (_data, components, mapImage, roadMap) => {
         <h6 style="font-size: 14px; color: #11093c; margin: 0; font-weight: 400;">${el.business_associate_contact.contact_name}</h6>
         <p style="font-size: 12px; color: #a09cb1; margin-bottom: 0px; margin-top: -3px; padding-top: 5px;">${STAFF_ROL_MAP[el.code_project_staff_role_type_id]}</p>
       </td>
-      <td style="width:  87.5px; text-align: right;">
+      <td style="width:  87.5px; text-align: right;
+      overflow:hidden; 
+text-overflow:ellipsis;
+display:-webkit-box; 
+-webkit-box-orient:vertical;
+-webkit-line-clamp:2; line-height: 15px; margin-top:5px;">
         <span style="font-size: 12px; color: #a09cb1; margin-bottom: 15px;margin-top: -3px; margin-right:16px;">${el.business_associate_contact.user.organization}</span>
       </td>
     </tr>
