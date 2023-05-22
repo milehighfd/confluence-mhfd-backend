eb78c93420e9710231db55c1de3226f3613bf635
melvin <melvincc78@gmail.com>
2023-05-22 12:41:06 -0400

    enable income filter

--- src/routes/financial.route.js
+++ src/routes/financial.route.js
@@ -7,39 +7,63 @@ const ProjectCost = db.projectCost;
 const ProjectPartner = db.projectPartner;
 const BusinessAssociates = db.businessAssociates;
 const codePhaseType = db.codePhaseType;
+const CodeCostType = db.codeCostType
 const router = express.Router();
 
 router.post('/get-costs-by-id/:id', [auth], async (req, res) => {
+    logger.info('get-costs-by-id for project financials');
     let { id } = req.params;
     let filters = req.body;
     const PARTNER_FILTER = filters[0];
     const PHASE_FILTER = filters[1];
+    const IS_INCOME = filters[2];
     const PROJECT_ACTIVE_STATE = 1;
-
-    logger.info('get-costs-by-id');
-    
+    let resProjectCost;
+    let where = {
+        project_id: id,
+        is_active: PROJECT_ACTIVE_STATE
+    }
+    if (PARTNER_FILTER) {
+        where.project_partner_id = PARTNER_FILTER
+    }
+    if (PHASE_FILTER) {
+        where.code_phase_type_id = PHASE_FILTER
+    }
     try {
-        let where = {
-            project_id: id,
-            is_active: PROJECT_ACTIVE_STATE
+        if (IS_INCOME && (IS_INCOME?.income !== true || IS_INCOME?.expense !== true)) {
+            let INCOME_STATE;
+            if (IS_INCOME?.income === true) {
+                INCOME_STATE = 1;
+            }
+            if (IS_INCOME?.expense === true) {
+                INCOME_STATE = 0;
+            }
+            let isIncomeState = await CodeCostType.findAll({
+                where: {
+                    is_income: INCOME_STATE
+                },
+                raw: true,
+                nest: true,
+                attributes: ['code_cost_type_id'],
+            });
+            let arrayValues = isIncomeState.map(a => a.code_cost_type_id);
+            where.code_cost_type_id = arrayValues;
         }
-        if (PARTNER_FILTER) {
-            where.project_partner_id = PARTNER_FILTER 
-        }
-        if (PHASE_FILTER) {
-            where.code_phase_type_id = PHASE_FILTER 
-        }
-        let resProjectCost = await ProjectCost.findAll({
+
+        resProjectCost = await ProjectCost.findAll({
             where: where,
             raw: true,
             nest: true,
             attributes: ['agreement_number', 'amendment_number', 'code_phase_type_id', 'project_partner_id'],
         });
+
+
         resProjectCost = resProjectCost.filter(function (element) {
             if (element.agreement_number !== null || element.amendment_number !== null || element.code_phase_type_id !== null || element.project_partner_id !== null) {
                 return element;
             }
         });
+
         await Promise.all(
             resProjectCost.map(async (projectCost) => {
                 if (projectCost.project_partner_id !== null) {
