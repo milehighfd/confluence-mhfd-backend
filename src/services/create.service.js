8964ea18475f58c62c3f77f02e469c654e5fdd9d
angel cussi <angel.cussi@vizonomy@vizonomy.com>
2023-05-25 15:31:46 -0400

    add real test case

--- src/services/create.service.js
+++ src/services/create.service.js
@@ -1,15 +1,27 @@
-import moment from "moment";
+import moment from 'moment';
 //Project
 class Project {
   constructor() {
-    this.name = "";
-    this.description = "";
-    this.creator = "";
-    this.creationDate = "";
+    this.project_name = '';
+    this.project_alias = '';
+    this.description = '';
+    this.onbase_project_number = 0;
+    this.location = '';
+    this.code_project_type_id = 0;
+    this.start_date = '';
+    this.staff_lead_email = '';
+    this.current_project_status_id = 0;
+    this.code_maintenance_eligibility_type_id = 0;
+    this.creator = '';
   }
 
-  setName(name) {
-    this.name = name;
+  setName(project_name) {
+    this.project_name = project_name;
+    return this;
+  }
+
+  setAlias(project_alias) {
+    this.project_alias = project_alias;
     return this;
   }
 
@@ -18,32 +30,77 @@ class Project {
     return this;
   }
 
+  setOnBase(onbase_project_number) {
+    this.onbase_project_number = onbase_project_number;
+    return this;
+  }
+
+  setLocation(location) {
+    this.location = location;
+    return this;
+  }
+
+  setCodeProjectTypeId(code_project_type_id) {
+    this.code_project_type_id = code_project_type_id;
+    return this;
+  }
+
+  setStartDate() {
+    this.start_date = moment().format();
+    return this;
+  }
+
+  setStaffLeadEmail(staff_lead_email) {
+    this.staff_lead_email = staff_lead_email;
+    return this;
+  }
+
+  setCurrentProjectStatusId(current_project_status_id) {
+    this.current_project_status_id = current_project_status_id;
+    return this;
+  }
+
+  setCodeMaintenanceEligibilityTypeId(code_maintenance_eligibility_type_id) {
+    this.code_maintenance_eligibility_type_id = code_maintenance_eligibility_type_id;
+    return this;
+  }
+
   setCreator(creator) {
     this.creator = creator;
     return this;
   }
 
-  setCreationDate() {
-    this.creationDate = moment().format();
-    return this;
-  }
+
 
   build() {
     return {
-      name: this.name,
+      project_name: this.project_name,
+      project_alias: this.project_alias,
       description: this.description,
+      onbase_project_number: this.onbase_project_number,
+      location: this.location,
+      code_project_type_id: this.code_project_type_id,
+      start_date: this.start_date,
+      staff_lead_email: this.staff_lead_email,
+      current_project_status_id: this.current_project_status_id,
+      code_maintenance_eligibility_type_id: this.code_maintenance_eligibility_type_id,
       creator: this.creator,
-      creationDate: this.creationDate,
     };
   }
 }
 
-
 const project = new Project()
-    .setName('test')
-    .setDescription('test desc')
-    .setCreator('test creator')
-    .setCreationDate()
-    .build();
+  .setName('test')
+  .setAlias('test alias')
+  .setDescription('test desc')
+  .setOnBase(123)
+  .setLocation('test location')
+  .setCodeProjectTypeId(123)
+  .setStartDate()
+  .setStaffLeadEmail('some@some.com')
+  .setCurrentProjectStatusId(123)
+  .setCodeMaintenanceEligibilityTypeId(123)
+  .setCreator('test creator')
+  .build();
 
-console.log(project)
+console.log(project);
