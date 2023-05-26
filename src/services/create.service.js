5b3ba78e30aeabbde1c2a9ea899f9182ef1ae8a8
angel cussi <angel.cussi@vizonomy@vizonomy.com>
2023-05-26 16:22:38 -0400

    return a new Builder project with build

--- src/services/create.service.js
+++ src/services/create.service.js
@@ -70,34 +70,63 @@ class ProjectBuilder {
     this.creator = creator;
     return this;
   }
-
   static build() {
-    return new ProjectBuilder({
-      project_name: this.project_name,
-      project_alias: this.project_alias,
-      description: this.description,
-      onbase_project_number: this.onbase_project_number,
-      location: this.location,
-      code_project_type_id: this.code_project_type_id,
-      start_date: this.start_date,
-      staff_lead_email: this.staff_lead_email,
-      current_project_status_id: this.current_project_status_id,
-      code_maintenance_eligibility_type_id:
-        this.code_maintenance_eligibility_type_id,
-      creator: this.creator,
-    });
+    return new Project(
+      this.project_name,
+      this.project_alias,
+      this.description,
+      this.onbase_project_number,
+      this.location,
+      this.code_project_type_id,
+      this.start_date,
+      this.staff_lead_email,
+      this.current_project_status_id,
+      this.code_maintenance_eligibility_type_id,
+      this.creator
+    );
   }
 }
 
-console.log(ProjectBuilder
+class Project {
+  constructor(
+    project_name,
+    project_alias,
+    description,
+    onbase_project_number,
+    location,
+    code_project_type_id,
+    start_date,
+    staff_lead_email,
+    current_project_status_id,
+    code_maintenance_eligibility_type_id,
+    creator
+  ) {
+    this.project_name = project_name;
+    this.project_alias = project_alias;
+    this.description = description;
+    this.onbase_project_number = onbase_project_number;
+    this.location = location;
+    this.code_project_type_id = code_project_type_id;
+    this.start_date = start_date;
+    this.staff_lead_email = staff_lead_email;
+    this.current_project_status_id = current_project_status_id;
+    this.code_maintenance_eligibility_type_id = code_maintenance_eligibility_type_id;
+    this.creator = creator;
+  }
+}
+
+console.log(
+  ProjectBuilder
   .setName('test')
-  .setAlias('test alias')
-  .setDescription('test desc')
-  .setOnBase(123)
-  .setLocation('test location')
-  .setCodeProjectTypeId(123)
-  .setStartDate()
-  .setStaffLeadEmail('some@some.com')
-  .setCurrentProjectStatusId(123)
-  .setCodeMaintenanceEligibilityTypeId(123)
-  .setCreator('test creator'));
+    .setAlias('test alias')
+    .setDescription('test desc')
+    .setOnBase(123)
+    .setLocation('test location')
+    .setCodeProjectTypeId(123)
+    .setStartDate()
+    .setStaffLeadEmail('some@some.com')
+    .setCurrentProjectStatusId(123)
+    .setCodeMaintenanceEligibilityTypeId(123)
+    .setCreator('test creator')
+    .build()
+);
