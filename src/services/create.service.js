7d35b287a8e8fcb039a1045933b9ab5c53989026
angel cussi <angel.cussi@vizonomy@vizonomy.com>
2023-05-26 16:40:46 -0400

    fix for capital tests

--- src/services/create.service.js
+++ src/services/create.service.js
@@ -15,62 +15,62 @@ class ProjectBuilder {
     this.creator = '';
   }
 
-  static setName(project_name) {
+  setName(project_name) {
     this.project_name = project_name;
     return this;
   }
 
-  static setAlias(project_alias) {
+  setAlias(project_alias) {
     this.project_alias = project_alias;
     return this;
   }
 
-  static setDescription(description) {
+  setDescription(description) {
     this.description = description;
     return this;
   }
 
-  static setOnBase(onbase_project_number) {
+  setOnBase(onbase_project_number) {
     this.onbase_project_number = onbase_project_number;
     return this;
   }
 
-  static setLocation(location) {
+  setLocation(location) {
     this.location = location;
     return this;
   }
 
-  static setCodeProjectTypeId(code_project_type_id) {
+  setCodeProjectTypeId(code_project_type_id) {
     this.code_project_type_id = code_project_type_id;
     return this;
   }
 
-  static setStartDate() {
+  setStartDate() {
     this.start_date = moment().format();
     return this;
   }
 
-  static setStaffLeadEmail(staff_lead_email) {
+  setStaffLeadEmail(staff_lead_email) {
     this.staff_lead_email = staff_lead_email;
     return this;
   }
 
-  static setCurrentProjectStatusId(current_project_status_id) {
+  setCurrentProjectStatusId(current_project_status_id) {
     this.current_project_status_id = current_project_status_id;
     return this;
   }
 
-  static setCodeMaintenanceEligibilityTypeId(code_maintenance_eligibility_type_id) {
-     this.code_maintenance_eligibility_type_id =
-      code_maintenance_eligibility_type_id;
-      return this;
+  setCodeMaintenanceEligibilityTypeId(code_maintenance_eligibility_type_id) {
+    this.code_maintenance_eligibility_type_id = code_maintenance_eligibility_type_id;
+    return this;
   }
 
-  static setCreator(creator) {
+  setCreator(creator) {
     this.creator = creator;
     return this;
   }
-  static build() {
+
+  build() {
     return new Project(
       this.project_name,
       this.project_alias,
@@ -114,10 +114,69 @@ class Project {
     this.creator = creator;
   }
 }
+class CapitalProject extends Project {
+  constructor(
+    project_name,
+    project_alias,
+    description,
+    onbase_project_number,
+    location,
+    code_project_type_id,
+    start_date,
+    staff_lead_email,
+    current_project_status_id,
+    code_maintenance_eligibility_type_id,
+    creator,
+    amount
+  ) {
+    super(
+      project_name,
+      project_alias,
+      description,
+      onbase_project_number,
+      location,
+      code_project_type_id,
+      start_date,
+      staff_lead_email,
+      current_project_status_id,
+      code_maintenance_eligibility_type_id,
+      creator
+    );
+    this.amount = amount;
+  }
+}
+
+class CapitalProjectBuilder extends ProjectBuilder {
+  constructor() {
+    super();
+  }
+
+  setAmount(amount) {
+    this.amount = amount;
+    return this;
+  }
+
+  build() {
+    return new CapitalProject(
+      this.project_name,
+      this.project_alias,
+      this.description,
+      this.onbase_project_number,
+      this.location,
+      this.code_project_type_id,
+      this.start_date,
+      this.staff_lead_email,
+      this.current_project_status_id,
+      this.code_maintenance_eligibility_type_id,
+      this.creator,
+      this.amount
+    );
+  }
+}
 
 console.log(
-  ProjectBuilder
-  .setName('test')
+  new CapitalProjectBuilder()
+    .setName('test')
     .setAlias('test alias')
     .setDescription('test desc')
     .setOnBase(123)
@@ -128,5 +187,6 @@ console.log(
     .setCurrentProjectStatusId(123)
     .setCodeMaintenanceEligibilityTypeId(123)
     .setCreator('test creator')
+    .setAmount(100000)
     .build()
 );
