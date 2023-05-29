dd51ab19eb59c1a63806d01d36ed1ef7d1141ff7
angel cussi <angel.cussi@vizonomy@vizonomy.com>
2023-05-29 16:33:10 -0400

    fix constructed

--- src/routes/new-project/index.js
+++ src/routes/new-project/index.js
@@ -73,7 +73,7 @@ router.post('/get-components-by-components-and-geom', auth,async (req, res) => {
     const problemid = component === 'stream_improvement_measure' ? 'problem_id' : 'problemid';
     const projectid = component === 'stream_improvement_measure' ? 'project_id' : 'projectid';
     const sql = `SELECT objectid, cartodb_id, ${type}, ${jurisdiction}, status, ${cost}, ${problemid}  FROM ${component} 
-    WHERE  ${queryWhere} AND ${projectid} is null `;
+    WHERE  ${queryWhere} AND ${projectid} is null AND status NOT LIKE '%Constructed%'`;
     const query = {
       q: sql
     };
