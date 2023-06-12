9b176703735de78b06f575910f72c0362690aa0f
Donwilo <burgoadanilson@gmail.com>
2023-06-12 12:04:44 -0400

    change date

--- src/routes/index.js
+++ src/routes/index.js
@@ -11,7 +11,7 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/echo', (_, res) => {
-  res.send({ echo: 'success', version: 'Jun 2 17:10' });
+  res.send({ echo: 'success', version: 'Jun 12 09:12' });
 });
 
 const server_style = function(req, res, next) {
