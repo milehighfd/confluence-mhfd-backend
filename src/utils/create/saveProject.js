efb07dbb5b9d834c4e73fa668249add7bfb44ad5
Lua <xdaddisxd@gmail.com>
2023-07-03 16:44:05 -0400

    save project generic

--- src/utils/create/saveProject.js
+++ src/utils/create/saveProject.js
@@ -2,50 +2,48 @@
 
 import db from 'bc/config/db.js';
 import logger from 'bc/config/logger.js';
+import moment from 'moment';
 
 const Project = db.project;
 
+
 export const saveProject = async (
-  CREATE_PROJECT_TABLE,
+  code_project_type_id,
   project_name,
   description,
-  code_project_type_id,
-  created_date,
-  modified_date,
-  start_date,
-  last_modified_by,
-  created_by,
+  creator,
   code_maintenance_eligibility_type_id = null,
-  transaction = null
+  transaction = null,
 ) => {
   try {
     let insert;
+    const currendDate = moment().format('YYYY-MM-DD HH:mm:ss');
     if (code_maintenance_eligibility_type_id) {
-      insert = Project.create({
+      insert = await Project.create({
         project_name: project_name,
         project_alias: project_name,
         description: description,
         code_project_type_id: code_project_type_id,
-        created_date: created_date,
-        modified_date: modified_date,
-        start_date: start_date,
-        last_modified_by: last_modified_by,
+        created_date: currendDate,
+        modified_date: currendDate,
+        start_date: currendDate,
+        last_modified_by: creator,
         is_spatial_data_required: 0,
-        created_by: created_by,
+        created_by: creator,
         code_maintenance_eligibility_type_id: code_maintenance_eligibility_type_id,
       }, { transaction: transaction });
     } else {
-      insert = Project.create({
+      insert = await Project.create({
         project_name: project_name,
         project_alias: project_name,
         description: description,
         code_project_type_id: code_project_type_id,
-        created_date: created_date,
-        modified_date: modified_date,
-        start_date: start_date,
-        last_modified_by: last_modified_by,
+        created_date: currendDate,
+        modified_date: currendDate,
+        start_date: currendDate,
+        last_modified_by: creator,
         is_spatial_data_required: 0,
-        created_by: created_by
+        created_by: creator
       }, { transaction: transaction });
     }
     logger.info('create project ');
@@ -55,3 +53,6 @@ export const saveProject = async (
     throw error;
   }
 }
+
+const CAPITAL_CODE_PROJECT_TYPE_ID = 5;
+export const saveCapital = saveProject.bind(null, CAPITAL_CODE_PROJECT_TYPE_ID);
