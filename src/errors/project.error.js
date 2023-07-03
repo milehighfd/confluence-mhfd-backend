470b3ed230816c44638c645d1cb62ac97319ed28
Lua <xdaddisxd@gmail.com>
2023-07-03 16:31:53 -0400

    add project errors

---
+++ src/errors/project.error.js
@@ -0,0 +1,61 @@
+export class ProjectError extends Error {
+  constructor(message) {
+    super(message);
+    this.name = 'ProjectError';
+  }
+}
+
+export class ProjectAttachmentError extends Error {
+  constructor(message) {
+    super(message);
+    this.name = 'ProjectAttachmentError';
+  }
+}
+
+export class ProjectBoardsError extends Error {
+  constructor(message) {
+    super(message);
+    this.name = 'ProjectBoardsError';
+  }
+}
+
+export class ProjectSponsorsError extends Error {
+  constructor(message, options) {
+    super(message, options);
+    this.name = 'ProjectSponsorsError';
+  }
+}
+
+export class ProjectLocalGovernmentError extends Error {
+  constructor(message, options) {
+    super(message, options);
+    this.name = 'ProjectLocalGovernmentError';
+  }
+}
+
+export class ProjectCountiesError extends Error {
+  constructor(message, options) {
+    super(message, options);
+    this.name = 'ProjectCountiesError';
+  }
+}
+
+export class ProjectServiceAreaError extends Error {
+  constructor(message, options) {
+    super(message, options);
+    this.name = 'ProjectServiceAreaError';
+  }
+}
+export class ProjectActionsError extends Error {
+  constructor(message, options) {
+    super(message, options);
+    this.name = 'ProjectActionsError';
+  }
+}
+
+export class ProjectCostsError extends Error {
+  constructor(message, options) {
+    super(message, options);
+    this.name = 'ProjectCostsError';
+  }
+}
