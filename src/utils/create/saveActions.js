49e62396fced7de2f0ca8e9d9a07398498713cd9
Donwilo <burgoadanilson@gmail.com>
2023-07-04 17:22:54 -0400

    add promise and return to saveactions

--- src/utils/create/saveActions.js
+++ src/utils/create/saveActions.js
@@ -2,12 +2,13 @@ import { saveProjectAction, saveProjectIndependentAction } from 'bc/utils/create
 import logger from 'bc/config/logger.js';
 import { ProjectActionsError } from 'bc/errors/project.error.js';
 
-export const saveActions = async (project_id, independetComponent, components, creator, transaction) => {
+export const saveActions = async (project_id, independentComponent, components, creator, transaction) => {
   try {
+    const promises = [];
     // Insert independent components
-    if (independetComponent) {
-      for (const independent of JSON.parse(independetComponent)) {
-        await saveProjectIndependentAction({
+    if (independentComponent) {
+      for (const independent of JSON.parse(independentComponent)) {
+        const promise = saveProjectIndependentAction({
           action_name: independent.name,
           project_id: project_id,
           cost: !isNaN(Number(independent.cost)) ? Number(independent.cost) : 0,
@@ -15,7 +16,7 @@ export const saveActions = async (project_id, independetComponent, components, c
           last_modified_by: creator,
           created_by: creator          
         }, transaction);
-        logger.info('create independent component');
+        promises.push(promise);
       }
     }
     // Insert proposed components
@@ -28,11 +29,29 @@ export const saveActions = async (project_id, independetComponent, components, c
           last_modified_by: creator,
           created_by: creator,
         };
-        await saveProjectAction(action, transaction);
-        logger.info('create component');
+        const promise = saveProjectAction(action, transaction);
+        promises.push(promise);
       }
     }
+    const result = await Promise.all(promises)
+      .then((results) => {
+        const resultObj = {};
+        for (const [index, result] of results.entries()) {
+          if (independentComponent && index < JSON.parse(independentComponent).length) {
+            resultObj[`independentCreated${index}`] = result;
+          } else {
+            resultObj[`componentCreated${index - JSON.parse(independentComponent).length}`] = result;
+          }
+        }
+        logger.info('All actions saved successfully');
+        return resultObj;
+      })
+      .catch((error) => {
+        logger.error('Error saving actions:', error);
+        throw ProjectActionsError('Error saving actions', { cause: error });
+      });
+    return result;
   } catch (error) {
-    throw ProjectActionsError('Error creating actions', { cause: error });
+    throw new ProjectActionsError('Error creating actions', { cause: error });
   }
 };
