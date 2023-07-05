f9369a35401e9b5c897b4f6d093a8c65fa524e5f
Donwilo <burgoadanilson@gmail.com>
2023-07-05 16:09:40 -0400

    fix create study

--- src/utils/create/saveStudy.js
+++ src/utils/create/saveStudy.js
@@ -6,34 +6,35 @@ const ProjectDetail = db.projectDetail;
 export const saveStudy = async (
   project_id,
   code_study_reason_id,
-  last_modified_by,
-  created_by,
-  transaction = null,
+  creator,
   otherReason = null,
+  transaction = null,  
 ) => {
   try {
-    await ProjectDetail.create({
-      maintenance_frequency: 0,
-      is_public_ownership: 0,
-      project_id: project_id,
-      code_study_reason_id: code_study_reason_id,
-      last_modified_by: last_modified_by,
-      created_by: created_by
-    }, { transaction });
-
+    let study = null;
     if (otherReason) {
-      await ProjectDetail.create({
+      study = await ProjectDetail.create({
         maintenance_frequency: 0,
         is_public_ownership: 0,
         project_id: project_id,
         comment: otherReason,
         code_study_reason_id: code_study_reason_id,
-        last_modified_by: last_modified_by,
-        created_by: created_by
+        last_modified_by: creator,
+        created_by: creator
+      }, { transaction });
+    }else{
+      study = await ProjectDetail.create({
+        maintenance_frequency: 0,
+        is_public_ownership: 0,
+        project_id: project_id,
+        code_study_reason_id: code_study_reason_id,
+        last_modified_by: creator,
+        created_by: creator
       }, { transaction });
     }
 
     logger.info('create Study ');
+    return { message: 'Study created successfully', study };
   } catch(error) {
     logger.error('error Study creation ', error);
     throw error;
