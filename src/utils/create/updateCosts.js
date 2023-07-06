3d4ad845ce2c4ee1519c9f5868944e257b04c898
Donwilo <burgoadanilson@gmail.com>
2023-07-06 14:45:45 -0400

    add transaction and promise to costs

--- src/utils/create/updateCosts.js
+++ src/utils/create/updateCosts.js
@@ -1,31 +1,47 @@
-import { 
+import {
   setCostActiveToFalse,
   saveProjectCost
 } from 'bc/utils/create';
+import { EditCostProjectError } from '../../errors/project.error.js';
+import logger from 'bc/config/logger.js';
 
-export const updateCosts = async (project_id, additionalcost, aditionalCostId, additionalcostdescription, creator, splitedOverheadcost, filtered, transaction) => {
-  await setCostActiveToFalse(project_id, transaction);
-  await saveProjectCost(
-    {
-      project_id,
+export const updateCosts = async (project_id, additionalcost, aditionalCostId, additionalcostdescription, creator, filtered, filterFrontOverheadCosts, transaction) => {
+  try {
+    await setCostActiveToFalse(project_id, transaction);
+    const promises = [];
+    //creating aditional cost
+    promises.push(saveProjectCost({
+      project_id: project_id,
       cost: !isNaN(Number(additionalcost)) ? Number(additionalcost) : 0,
       code_cost_type_id: aditionalCostId,
-      cost_description: additionalcostdescription || null,
+      cost_description: additionalcostdescription,
       created_by: creator,
       modified_by: creator,
-      is_active: true
-    }, transaction
-  );    
-  filtered.forEach(async (element, index) => {
-    await saveProjectCost(
-      {
-        project_id,
-        cost: !isNaN(Number(splitedOverheadcost[index+1])) ? Number(splitedOverheadcost[index+1]) : 0,
+      is_active: true,
+    }, transaction));
+    //creating overhead cost
+    for (const [index, element] of filtered.entries()) {
+      promises.push(saveProjectCost({
+        project_id: project_id,
+        cost: !isNaN(Number(filterFrontOverheadCosts[index])) ? Number(filterFrontOverheadCosts[index]) : 0,
         code_cost_type_id: element,
         created_by: creator,
         modified_by: creator,
-        is_active: true
-      }, transaction
-    );
-  });
+        is_active: true,
+      }, transaction));
+    }
+    const result = await Promise.all(promises)
+      .then((results) => {
+        logger.info('All costs saved successfully');
+        return results;
+      })
+      .catch((error) => {
+        logger.error('Error saving costs:', error);
+        throw new ProjectCostsError('Error saving costs', { cause: error });
+      });
+    return result;
+  } catch (error) {
+    console.error(error);
+    throw new EditCostProjectError('Error creating project or creatings statuses', { cause: error });
+  }
 }
