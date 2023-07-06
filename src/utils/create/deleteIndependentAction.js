bb9a78e26f4cbf975cb905d9a1d24843a2aad0e7
Donwilo <burgoadanilson@gmail.com>
2023-07-06 14:43:52 -0400

    add transactions to delete

--- src/utils/create/deleteIndependentAction.js
+++ src/utils/create/deleteIndependentAction.js
@@ -3,16 +3,23 @@ import logger from 'bc/config/logger.js';
 
 const ProjectIndependentAction = db.projectIndependentAction;
 
-export const deleteIndependentAction= async (project_id) => {
-  const project = await ProjectIndependentAction.destroy({
-    where: {
-      project_id: project_id 
-    }});
-  if (project) {
-    logger.info('ProjectIndependentAction destroyed ');
-    return true;
-  } else {
-    logger.info('ProjectIndependentAction not found');
-    return false;
+export const deleteIndependentAction = async (project_id, transaction) => {
+  try {
+    const project = await ProjectIndependentAction.destroy({
+      where: {
+        project_id: project_id
+      },
+      transaction
+    });
+    if (project) {
+      logger.info('ProjectIndependentAction destroyed ');
+      return true;
+    } else {
+      logger.info('ProjectIndependentAction not found');
+      return false;
+    }
+  } catch (error) {
+    logger.error(`Error deleting independent action: ${error}`);
+    throw error;
   }
 }
--- src/utils/create/deleteProposedAction.js
+++ src/utils/create/deleteProposedAction.js
@@ -3,16 +3,23 @@ import logger from 'bc/config/logger.js';
 
 const ProjectProposedAction = db.projectProposedAction;
 
-export const deleteProposedAction = async (project_id) => {
-  const project = ProjectProposedAction.destroy({
-    where: {
-      project_id: project_id 
-    }});
-  if (project) {
-    logger.info('ProjectComponent destroyed ');
-    return true;
-  } else {
-    logger.info('ProjectComponent not found');
-    return false;
+export const deleteProposedAction = async (project_id, transaction) => {
+  try {
+    const project = await ProjectProposedAction.destroy({
+      where: {
+        project_id: project_id 
+      },
+      transaction
+    });
+    if (project) {
+      logger.info('ProjectComponent destroyed');
+      return true;
+    } else {
+      logger.info('ProjectComponent not found');
+      return false;
+    }
+  } catch (error) {
+    logger.error(`Error deleting proposed action: ${error}`);
+    throw error;
   }
 }
