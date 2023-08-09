1ac5ccb987fb3e368f0468c855f3cc54cb8d9e4b
Donwilo <burgoadanilson@gmail.com>
2023-08-09 16:21:15 -0400

    Add boards relations

--- src/config/db.js
+++ src/config/db.js
@@ -201,6 +201,8 @@ db.landscapingArea.hasMany(db.projectStaff, {foreignKey: 'project_id', sourceKey
 db.streamImprovementMeasure.hasMany(db.projectStaff, {foreignKey: 'project_id', sourceKey: 'project_id'});
 //Boards
 db.boardProject.hasOne(db.project, {foreignKey: 'project_id', sourceKey: 'project_id', as: 'projectData'});
+db.project.hasMany(db.boardProject, {foreignKey: 'project_id', sourceKey: 'project_id'});
+db.boardProject.hasOne(db.codeStatusType, {foreignKey: 'code_status_type_id', sourceKey: 'code_status_type_id'});
 db.boardProjectCost.hasMany(db.boardProject, {foreignKey: 'board_project_id'});
 db.boardProjectCost.hasMany(db.projectCost, {foreignKey: 'project_cost_id'});
 //Notifications
