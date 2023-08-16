ee03fe3dbb9df2e438a4677699c2bbfe5f388643
JORGE <egrojmonroy@gmail.com>
2023-08-16 09:48:07 -0400

    add log for parse json

--- src/utils/create/insertIntoArcGis.js
+++ src/utils/create/insertIntoArcGis.js
@@ -33,6 +33,7 @@ export const insertIntoArcGis = async (geom, projectid, projectname) => {
     const fd = getAuthenticationFormData();
     const token_data = await needle('post', URL_TOKEN, fd, { multipart: true });
     const TOKEN = JSON.parse(token_data.body).token;
+    console.log('ERROR AT PARSING JSON', geom);
     const bodyFD = createGeomDataForARCGIS(JSON.parse(geom).coordinates, TOKEN, projectid);
     console.log('About to call endpoint: ', `${ARCGIS_SERVICE}/applyEdits`);
     const createOnArcGis = await needle('post',`${ARCGIS_SERVICE}/applyEdits`, bodyFD, { multipart: true });
