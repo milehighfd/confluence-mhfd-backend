810cd1d88b21ac7368913b2dffb119492ea28534
Donwilo <burgoadanilson@gmail.com>
2023-08-30 14:20:55 -0400

    change build date to locale string

--- src/generateDate.js
+++ src/generateDate.js
@@ -1,5 +1,5 @@
 import fs from 'fs/promises';
 (async function fillDate() {
-  const date = new Date().toISOString();
+  const date = new Date().toLocaleString();
   await fs.writeFile('./src/DATEFILE', date);
 })();
