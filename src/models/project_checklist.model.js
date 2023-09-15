60371b3a589aaf9adcdeb5007f8112d2e8c90e0e
Donwilo <burgoadanilson@gmail.com>
2023-09-15 12:49:25 -0400

    Add checklist model

---
+++ src/models/project_checklist.model.js
@@ -0,0 +1,40 @@
+export default (sequelize, DataTypes) => {
+    const ProjectChecklist = sequelize.define('project_checklist', {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+        },
+        project_id: {
+            type: DataTypes.INTEGER,
+        },
+        phase_type_id: {
+            type: DataTypes.INTEGER,
+        },
+        checklist_todo_name: {
+            type: DataTypes.STRING,
+        },
+        is_completed: {
+            type: DataTypes.BOOLEAN,
+        },
+        created_by: {
+            type: DataTypes.STRING,
+        },
+        updated_by: {
+            type: DataTypes.STRING,
+        },
+        created_at: {
+            type: DataTypes.DATE,
+        },
+        updated_at: {
+            type: DataTypes.DATE,
+        },
+    }, {
+        freezeTableName: true,
+        tableName: 'project_checklist',
+        createdAt: 'created_at',
+        updatedAt: 'updated_at',
+        underscored: true,
+    });
+
+    return ProjectChecklist;
+};
