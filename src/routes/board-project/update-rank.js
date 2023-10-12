6661321b62179a9344baa1d79ecf1c6ffa1e4ce0
Donwilo <burgoadanilson@gmail.com>
2023-10-12 17:23:01 -0400

    Fix Allow drag and drop and add costs

--- src/routes/board-project/update-rank.js
+++ src/routes/board-project/update-rank.js
@@ -7,19 +7,75 @@ import { LexoRank } from 'lexorank';
 import moment from 'moment';
 
 const BoardProject = db.boardProject;
+const BoardProjectCost = db.boardProjectCost;
+const ProjectCost = db.projectCost;
+const ProjectPartner = db.projectPartner;
 const { Op } = sequelize;
+
+async function getBoardProjectCost(board_project_id, columnNumber) {
+  const MHFD_CODE_PARTNER_TYPE = 88;
+  const reqExist = await BoardProjectCost.findOne({
+    attributes: ['req_position', 'board_project_id'],
+    where: {
+      board_project_id,
+      req_position: columnNumber
+    },
+    include: [
+      {
+        attributes: ['cost', 'code_cost_type_id', 'project_partner_id','project_id','project_cost_id'],
+        model: ProjectCost,
+        as: 'projectCostData',
+        where: {
+          is_active: true,
+        },
+        is_required: true,
+        include: [
+          {
+            model: ProjectPartner,
+            as: 'projectPartnerData',
+            is_required: true,
+            where: {
+              code_partner_type_id: MHFD_CODE_PARTNER_TYPE
+            }
+          }
+        ]
+      }
+    ]
+  });
+  return reqExist;
+}
+
+function getNumberFromRank(otherFields) {
+  if (!otherFields) {
+    return null;
+  }
+  for (let i = 0; i <= 5; i++) {
+    const rankKey = `rank${i}`;
+    if (otherFields.hasOwnProperty(rankKey)) {
+      return i;
+    }
+  }
+  return null;
+}
 
 const insertOnColumnAndFixColumn = async (columnNumber, board_id, targetPosition, otherFields, board_project_id, user) => {
   console.log('here');
   const reqColumnName = `req${columnNumber}`;
   const rankColumnName = `rank${columnNumber}`;
   const where = { board_id };
+  let reqExist = null;
   if (`${columnNumber}` !== '0') {
-    where[reqColumnName] = { [Op.ne]: null };
+    reqExist = await getBoardProjectCost(board_project_id, columnNumber);
+    console.log('reqExist', reqExist)
+    //where[reqColumnName] = { [Op.ne]: null };
+    if (reqExist) {
+      where['board_project_id'] = reqExist.board_project_id;
+    }
   } else {
     where[rankColumnName] = { [Op.ne]: null }
   }
-
+  console.log('where', where)
+  console.log('req', reqExist)
   const projects = await BoardProject.findAll({
     where,
     order: [[rankColumnName, 'ASC']],
@@ -44,6 +100,8 @@ const insertOnColumnAndFixColumn = async (columnNumber, board_id, targetPosition
         if (keys.includes('req')) {
           const costToUpdate = otherFields[keys] ? otherFields[keys] : 0;
           const columnToEdit = keys.match(/[0-9]+/);
+          console.log(columnToEdit, 'columnToEdit')
+          console.log(costToUpdate, 'costToUpdate')
           await boardService.updateAndCreateProjectCosts(
             columnToEdit,
             costToUpdate,
@@ -56,13 +114,35 @@ const insertOnColumnAndFixColumn = async (columnNumber, board_id, targetPosition
           multiplicator++;
         }
       }
-    }
+    }   
     return await BoardProject.update(
       { [rankColumnName]: lastLexo },
       { where: { board_project_id: project.board_project_id } }
     );
   });
-  return Promise.all(proms);
+  let originCost = null;
+  let targetCost = null;
+  if (getNumberFromRank(otherFields) && columnNumber) {
+    originCost = await getBoardProjectCost(board_project_id, getNumberFromRank(otherFields));
+    targetCost = await getBoardProjectCost(board_project_id, columnNumber);
+    if (originCost && targetCost) {
+      const cost = originCost.projectCostData.cost + targetCost.projectCostData.cost;
+      proms.push(ProjectCost.update(
+        { cost: cost },
+        { where: { project_cost_id: targetCost.projectCostData.project_cost_id } }
+      ));
+      proms.push(ProjectCost.update(
+        { is_active: false },
+        { where: { project_cost_id: originCost.projectCostData.project_cost_id } }
+      ));
+    }
+  }
+  try {
+    const results = await Promise.all(proms);
+    console.log('results', results);
+  } catch (error) {
+    console.error(`Error updating ranks: ${error}`);
+  }
 };
 
 const updateRank = async (req, res) => {
@@ -149,12 +229,18 @@ const updateRank = async (req, res) => {
     console.log('onWorkspace', onWorkspace);
 
     for (const key in otherFields) {
-      if (key.includes('req') && key != 'req0') {
+      if (key != 'rank0') {
         const costToUpdate = otherFields[key] ? otherFields[key] : 0;
         const columnToEdit = key.match(/\d+/)[0];
+        const originCost = await getBoardProjectCost(board_project_id, getNumberFromRank(otherFields));
+        const targetCost = await getBoardProjectCost(board_project_id, columnNumber);
+        let cost = 0;
+        if (originCost && targetCost) {
+          cost = originCost.projectCostData.cost + targetCost.projectCostData.cost;
+        }
         await boardService.updateAndCreateProjectCosts(
           columnToEdit,
-          costToUpdate,
+          cost,
           boardProject.project_id,
           user,
           board_project_id,
