710b458af11f5cb7ff25a9dcad113d40665f3ea1
Donwilo <burgoadanilson@gmail.com>
2023-10-18 16:30:13 -0400

    add try catch to saveboard

--- src/utils/create/saveBoard.js
+++ src/utils/create/saveBoard.js
@@ -13,17 +13,22 @@ export const saveBoard = async (
   transaction = null
 ) => {
   const DRAFT_STATUS = 1;
-  const response = await BoardProject.create({
-    board_id: board_id,
-    project_id: project_id,
-    origin: origin,
-    rank0: rank0,
-    projectname: projectname,
-    code_status_type_id: DRAFT_STATUS,
-    createdAt: moment().format('YYYY-MM-DD HH:mm:ss'),
-    updatedAt: moment().format('YYYY-MM-DD HH:mm:ss'),
-    created_by: creator,
-    last_modified_by: creator
-  }, { transaction: transaction });
-  return response;
+  try {
+    const response = await BoardProject.create({
+      board_id: board_id,
+      project_id: project_id,
+      origin: origin,
+      rank0: rank0,
+      projectname: projectname,
+      code_status_type_id: DRAFT_STATUS,
+      createdAt: moment().format('YYYY-MM-DD HH:mm:ss'),
+      updatedAt: moment().format('YYYY-MM-DD HH:mm:ss'),
+      created_by: creator,
+      last_modified_by: creator
+    }, { transaction: transaction });
+    return response;
+  } catch (error) {
+    console.error(`Error saving board project: ${error}`);
+    throw error;
+  }
 }
