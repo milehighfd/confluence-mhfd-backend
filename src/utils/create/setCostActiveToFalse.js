aed9ff8f184c4685e6665a3f6fa674b4fcf5f1c0
JORGE <egrojmonroy@gmail.com>
2023-11-09 17:02:18 -0400

    add lastmodified on setting inactive overheadcost

--- src/utils/create/setCostActiveToFalse.js
+++ src/utils/create/setCostActiveToFalse.js
@@ -1,6 +1,7 @@
 import db from 'bc/config/db.js';
 import logger from 'bc/config/logger.js';
 import { Op } from 'sequelize';
+import moment from 'moment';
 const ProjectCost = db.projectCost;
 
 export const getCostActiveForProj = async (project_id, code_cost_type_ids, transaction = null) => {
@@ -26,6 +27,7 @@ export const setCostActiveToFalse = async (project_id, code_cost_type_id, transa
     await ProjectCost.update(
       {
         is_active: false,
+        last_modified: moment().format('YYYY-MM-DD HH:mm:ss')
       },
       {
         where: {
@@ -47,7 +49,8 @@ export const updateDescriptionOnly = async (project_id, cost_type_id, newDescrip
   try {
     await ProjectCost.update(
       {
-        cost_description: newDescription
+        cost_description: newDescription,
+        last_modified: moment().format('YYYY-MM-DD HH:mm:ss')
       },
       {
         where: {
