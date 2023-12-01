16b080c5bb301e3e6c8a625e049a6de3f3b9a3b2
JORGE <egrojmonroy@gmail.com>
2023-12-01 12:07:53 -0400

    create project with cost null because is in workspace

--- src/utils/create/createProjectWorkflow.js
+++ src/utils/create/createProjectWorkflow.js
@@ -35,7 +35,8 @@ import {
   saveVegetationManagement,
   cleanStringValue,
   saveSubtotalcost,
-  saveEstimatedCost
+  saveEstimatedCost,
+  saveWorkspaceCostInit
 } from 'bc/utils/create';
 import db from 'bc/config/db.js';
 import { ProjectError, ProjectBoardsError} from '../../errors/project.error.js';
@@ -304,7 +305,7 @@ export const createProjectWorkflow = async (body, user, files, type, subtype) =>
   try {    
     const data = await createProjects(body, transaction, type, user.email, subtype);
     const { project_id } = data;
-    const { cover, sponsor, cosponsor } = body;
+    const { cover, sponsor, cosponsor, isWorkPlan } = body;
     const project_attachments = await uploadFiles(user, files, project_id, cover, transaction);
     await addToBoard(body, user, type, subtype, transaction, project_id);
     const geoInfo = await parseGeographicInfoAndCreate(body, project_id, user, transaction);
@@ -330,7 +331,15 @@ export const createProjectWorkflow = async (body, user, files, type, subtype) =>
       },
     });
     console.log('\n-*******************- \n Last Project created should be: ', project_id, lastProject);
-
+    const WORK_PLAN_CODE_COST_TYPE_ID = 21;
+    const WORK_REQUEST_CODE_COST_TYPE_ID = 22;
+    await saveWorkspaceCostInit(
+      project_id,
+      boardProjectId.board_project_id,
+      isWorkPlan ? WORK_PLAN_CODE_COST_TYPE_ID: WORK_REQUEST_CODE_COST_TYPE_ID,
+      user.email
+    );
+    
     const composeData = { ...data, project_attachments, project_partner, ...geoInfo, extra_fields, lastProject, boardProjectId};
     return composeData;
   } catch (error) {
---
+++ src/utils/create/saveWorkspaceCost.js
@@ -0,0 +1,34 @@
+import { saveProjectCost } from 'bc/utils/create';
+import db from 'bc/config/db.js';
+import { ProjectCostsError } from '../../errors/project.error.js';
+import { CODE_DATA_SOURCE_TYPE } from 'bc/lib/enumConstants.js';
+const BoardProjectCost = db.boardProjectCost;
+export const saveWorkspaceCostInit = async (project_id, board_project_id, code_cost_type_id, creator, transaction) => { 
+  try {
+    const dataProjectCost = {
+      project_id: project_id,
+      cost: null,
+      code_cost_type_id: code_cost_type_id,
+      created_by: creator,
+      modified_by: creator,
+      is_active: true,
+      code_data_source_type_id: CODE_DATA_SOURCE_TYPE.SYSTEM
+    };
+    console.log('Null cost is created with values', dataProjectCost);
+    const projectCostCreated = await saveProjectCost(dataProjectCost, transaction);
+    const project_cost_id = projectCostCreated.dataValues.project_cost_id;
+    const currentColumn = 0;
+    const boardProjectCostCreated = await BoardProjectCost.create({
+        board_project_id: board_project_id,
+        project_cost_id: project_cost_id,
+        req_position: currentColumn,
+        created_by: creator,
+        last_modified_by: creator
+    });  
+    return boardProjectCostCreated;
+  } catch (error) {
+    console.error('FAIL at SAVE WORKSPACE COST INIT', error);
+    return [];
+  }
+  
+};
