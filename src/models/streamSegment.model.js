cde083b8a16a71627557e719ff18b38ac0dbd694
Donwilo <burgoadanilson@gmail.com>
2024-01-09 09:15:20 -0400

    Add streamSegment model

---
+++ src/models/streamSegment.model.js
@@ -0,0 +1,31 @@
+export default (sequelize, DataType) => {
+  const StreamSegment = sequelize.define('streamSegment', {
+    stream_id: {
+      type: DataType.INTEGER,
+      primaryKey: true
+    },
+    OBJECTID: {
+      type: DataType.INTEGER,
+      primaryKey: false
+    },
+    stream_name: {
+      type: DataType.STRING,
+      allowNull: false
+    },
+    mhfd_code_stream: {
+      type: DataType.STRING,
+      allowNull: false
+    },
+    mhfd_code_segment: {
+      type: DataType.STRING,
+      allowNull: false
+    },
+  }, {
+    freezeTableName: true,
+    tableName: 'STREAM_SEGMENT',
+    createdAt: false,
+    updatedAt: false  
+  }
+    );
+  return StreamSegment;
+}
